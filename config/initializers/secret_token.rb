5ab917ab1ad01a3782c9bbe683372a896795bc82
Pat Coleman <padsterpat@gmail.com>
2011-07-03 14:05:24 +1000

    all there

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DriftingBottle::Application.config.secret_token = 'c44b19dbd770733fb6ee0d5cc68960f2f588ea958439b0a3cafa5ef54dcca134c86412fdc34395bd6931247c3fbf22c688a972c07c14567ee5fbf371c34ddf9a'
